c3697b20a213044b5f0700cf42a2fda819515047
Josh-lib <joshuv037@gmail.com>
2022-06-07 08:25:39 +0300

    Update

--- Exercise_convert_km_to_miles.py
+++ Exercise_convert_km_to_miles.py
@@ -1,11 +1,12 @@
 
 from ast import Str
 
-#--Question:I want you to create a program that will get an input 
-#from the user that will be a number of kilometers.
-name_1 = str(input("Hello I am the caliki!! \n What is your name? " ))
+# --Question:I want you to create a program that will get an input
+# from the user that will be a number of kilometers.
+name_1 = str(input("Hello I am the caliki!! \n What is your name? "))
 var_1 = [name_1]
-num2 = float(input("Please enter the number of Km that will converted to Miles :"))
+num2 = float(
+    input("Please enter the number of Km that will converted to Miles :"))
 const_num1 = float(1.609344)
 ans_1 = (num2/const_num1)
-print ("Mr.",name_1,"I would like to inform you that they are: ", ans_1, "miles")
+print("Mr.", name_1, "I would like to inform you that they are: ", ans_1, "miles")
