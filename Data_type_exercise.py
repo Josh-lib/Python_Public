fad8342b7ca55e7d44068206d6f9122a21d64d01
Josh-lib <joshuv037@gmail.com>
2022-06-07 07:31:38 +0300

    Data type

--- Data_type_exercise.py
+++ Data_type_exercise.py
@@ -8,4 +8,14 @@ lname = input("Enter your Last name: ")
 print ("Your initials are",fname,mname,lname)
 
 #--Question Two--#
+#--Let say your company has aproduct with
+#--this lot number:"037-00901-00027".
+# create a program to print:
+# Country code:___
+# Product code:___
+# Batch number:___
 
+lot_num = "037-00901-00027"
+print ("Country Code:",lot_num[0:3])
+print ("Product code: ",lot_num[4:9])
+print ("Batch number: ",lot_num[10:15])
