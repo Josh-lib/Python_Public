2ca127893364acc6c601c77ada1e7b79297cbdad
Josh-lib <joshuv037@gmail.com>
2022-07-15 23:49:21 +0300

    Latest

--- Exercise - List and Tuples.py
+++ Exercise - List and Tuples.py
@@ -4,6 +4,8 @@
 
 month = ('January','Feburary','March','April','May','June','July','August','September','October','November','December')
 Date = input("When were you born, enter in this format DD-MM-YYYY:")
+
 entry = int(Date[3:5])-1
 date_of_birth = month[entry]
-print(date_of_birth)
+
+print("You were Born in ", date_of_birth)
