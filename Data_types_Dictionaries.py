9ac7b406a1ac393f4df995021488c70fcc1afd72
Josh-lib <joshuv037@gmail.com>
2022-07-22 09:25:19 +0300

    Updates for Data_types

---
+++ Data_types_Dictionaries.py
@@ -0,0 +1,7 @@
+People = {"nation": "Baganda", "colour": "Black", "Eyes": "Blue"}
+People["country"] = "USA", "Chile"
+People["Climate"] = ["Spring", "Summer"]
+People["Climate"].append("Cloudy")
+People["Birthday"] = 1955
+
+print(People)
