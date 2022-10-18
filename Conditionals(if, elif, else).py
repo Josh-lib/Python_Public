ced74b8186235a19fba279fd30b1b64f87757d80
Josh-lib <joshuv037@gmail.com>
2022-10-18 06:26:09 +0300

    Conditional statements Updates

---
+++ Conditionals(if, elif, else).py
@@ -0,0 +1,21 @@
+from asyncio import constants
+from cgi import print_form
+grade1 =float (input ("Type the grade of the first test: "))
+grade2 =float (input ("Type the grade of the second test: "))
+absences =int (input ("Type the number of absences: "))
+total_classes = int(input ("Type the total number of classes: "))
+constant = float(2)
+
+avg_grade = (grade1 + grade2) / (constant)
+attendance = (total_classes - absences) / total_classes
+Grade = print("The average grade", round(avg_grade,2))
+Attend = print ("The attendance_rate", str(round((attendance*100),2))+'%')
+if (avg_grade >= 6):
+    if (attendance >= 0.8):
+        print("The student has been approved")        
+    else:
+            print("This student has failed due to an attendance rate lower than 80%.")
+elif (attendance >= 0.8):
+    print("The student has failed due to an average grade lower than 6.0.")
+else:
+    print("The student has failed due to an average grade lower than 6.0 and an attendance rate lower than 80%.")
